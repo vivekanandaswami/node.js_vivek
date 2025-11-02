1de01ff3bc2886f1f5262f5f75f12cef4f237b63
vivekanandaswami <vivekananda29055@gmail.com>
2025-11-02 12:26:34 +0000

    datatype

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,36 @@
+"use strict";// treate js code as modern js code//
+
+alert( 3+1) // we are using alert function to show a message in the browser and we are using javascript //
+
+console( 3
+    +1 
+) // make code more readablity by proper indentation//
+
+console.log("hello world") 
+
+// different data types in javascript //
+// primitive data types// => string, number, boolean, undefined, null, bigint, symbol//
+           
+// //let name = "vivek" // string datatype//
+// let age = 25 // number datatype//
+// let isStudent = true // boolean datatype//
+// let address // undefined datatype//
+// let phoneNumber = null // null datatype// or standalone value//
+// let bigintNumber = 1234567890123456789012345678901234567890n // bigint datatype//
+// let symbolValue = Symbol("uniqueIdentifier") // symbol datatype//
+
+
+// non-primitive data types// => object, array, function//
+
+// let person = {  // object datatype//
+//     name: "vivek",
+//     age: 25,
+//     isStudent: true
+// }
+// array datatype//
+// let colors = ["red", "green", "blue"] // array datatype//
+// function datatype//
+// function greet() {  // function datatype//
+//     console.log("Hello!") 
+// }
+
