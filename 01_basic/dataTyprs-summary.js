c431c98981a72533afd326cfad6ec6cae549e9b3
vivekanandaswami <vivekananda29055@gmail.com>
2025-11-03 07:07:16 +0000

    dataType-summary

---
+++ 01_basic/dataTyprs-summary.js
@@ -0,0 +1,34 @@
+// different data types in javascript //
+// 7 types of data types in javascript //
+
+
+// primitive data types// => string, number, boolean, undefined, null, bigint, symbol//
+           
+//let name = "vivek" // string datatype//
+let age = 25 // number datatype//
+let isStudent = true // boolean datatype//
+let address // undefined datatype//
+let phoneNumber = null // null datatype// or standalone value//
+let bigintNumber = 1234567890123456789012345678901234567890n // bigint datatyp e//
+let symbolValue = Symbol("uniqueIdentifier") // symbol datatype//
+
+
+//  JAVASCERIPT IS DYNAMICALLY TYPED LANGUAGE//
+// WE DONT NEED TO SPECIFY DATA TYPE WHILE DECLARING A VARIABLE//
+// DATA TYPE WILL BE ASSIGNED AUTOMATICALLY BASED ON THE VALUE//
+
+
+// (Reference)or non-primitive data types// => object, array, function//
+
+// let person = {  // object datatype//
+//     name: "vivek",
+//     age: 25,
+//     isStudent: true
+// }
+// array datatype//
+// let colors = ["red", "green", "blue"] // array datatype//
+// function datatype//
+// function greet() {  // function datatype//
+//     console.log("Hello!") 
+// }
+
