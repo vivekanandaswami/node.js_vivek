61a4b7a4add5ca10ed428797da4887660517e724
vivekanandaswami <vivekananda29055@gmail.com>
2025-11-03 05:18:44 +0000

    converstion of dataType

--- 01_basic/03_converstionoperation.js
+++ 01_basic/03_converstionoperation.js
@@ -1,9 +1,9 @@
 let score = 33
-console.log(typeof (score))
-console.log("Score as number: " + score)
-let valueINumber = Number(score)
-console.log(typeof (valueINumber))
-console.log(valueINumber)
+// console.log(typeof (score))
+// console.log("Score as number: " + score)
+// let valueINumber = Number(score)
+// console.log(typeof (valueINumber))
+// console.log(valueINumber)
 
 //"33" => 33//
 //"33abc" => NaN//
@@ -16,8 +16,8 @@ console.log(valueINumber)
 
 let isloggedIn = "vivek"
 
-let booleanIsloggedIn = Boolean(isloggedIn)
-console.log(booleanIsloggedIn)
+// let booleanIsloggedIn = Boolean(isloggedIn)
+// console.log(booleanIsloggedIn)
 
 //1 => true to 1 , false to 0//
 //null => 0//
@@ -28,5 +28,65 @@ console.log(booleanIsloggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
-console.log(stringNumber)
+// console.log(typeof stringNumber)
+// console.log(stringNumber)
+
+
+
+
+//**************************operation ************************************//
+
+let valueINumber = 3
+let negativeNumber = -valueINumber
+// console.log(negativeNumber);
+
+//console.lof(3+3)//addtion//
+//console.log(2-2)//subtraction//
+//console.log(2*3)//multiplication//
+//console.log(10/2)//division//
+//console.log(10%3)//modulus operator//
+//console.log(2**3)//exponentiation operator 2^3=8//
+
+// ********************** abstract operation **************************//
+
+// + => addition//
+// - => subtraction//
+// * => multiplication//
+// / => division//
+// % => modulus//
+// ** => exponentiation//
+
+// console.log("3" + 3)//"33"//
+// console.log("3" - 3)//0//
+// console.log("3" * 3)//9//
+// console.log("6" / 2)//3//
+// console.log("10" % 3)//1//
+// console.log("2" ** 3)//8//
+
+// console.log(true + 3)//4//
+// console.log(false + 3)//3//
+// console.log(true - 3)//-2//
+// console.log(false - 3)//-3//
+// console.log(true * 3)//3//
+// console.log(false * 3)//0//
+// console.log(true / 3)//0.3333//
+// console.log(false / 3)//0//
+
+// ********************** increment and decrement operator ***************//
+
+let numcount= 33
+numcount++;
+console.log(numcount);
+numcount--;
+console.log(numcount);
+
+
+
+
+
+
+// link to learn more about conversion and operations and increment and decrement operator//
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment_and_Decrement_Operators
+//https://tc39.es/ecma262/2023/multipage/abstract-operations.html#sec-abstract-operations
+
+
