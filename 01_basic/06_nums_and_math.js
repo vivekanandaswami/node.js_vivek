d338151ebc3171365b119c3d8588aac94cc83874
vivekanandaswami <vivekananda29055@gmail.com>
2025-11-05 18:27:00 +0000

    num and math

---
+++ 01_basic/06_nums_and_math.js
@@ -0,0 +1,48 @@
+
+
+const score = 100;
+// console.log(score)
+
+// const namem = new Number("200");
+
+// console.log(typeof score);
+
+// console.log(namem.toString().length);
+
+// console.log(namem.toFixed(2));
+
+const price = 99.99;
+// console.log(price.toFixed(1));
+
+const largeNumber = 12345678901234567890n;
+// console.log(largeNumber);
+
+const ontherNumber = 122.5565
+// console.log(ontherNumber.toPrecision(4));
+
+const number= 10000000
+// console.log(number.toLocaleString("en-IN"));
+
+// console.log(Number.isInteger(123));
+// console.log(Number.isInteger(12.3));
+
+// console.log(Number.isNaN(NaN));
+// console.log(Number.isNaN("hello"));
+
+//++++++++++++++++++++++++ maths +++++++++++++++++++++++++++++++++++++++//
+
+console.log(Math.PI);
+console.log(Math)
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.7));
+console.log(Math.round(4.5));
+console.log(Math.trunc(4.9));
+
+console.log(Math.min(1,4,2,6,3,-1));
+console.log(Math.max(1,4,2,6,3,-1));
+
+console.log(Math.random());
+
+console.log(Math.random()*10);
+
+console.log((Math.random()*10)+1);
